299b0ba0c8a4f0e2c04e2c6350182b1299963ac4
Sunip Mukherjee <sunipkmukherjee@gmail.com>
2020-02-07 12:43:38 -0500

    Investigating hang

--- test/test.c
+++ test/test.c
@@ -619,7 +619,7 @@ void checkTransition(void)
              printf("[DETUMBLE]\n");
              fflush(stdout);
             next_mode = 1;
-            // g_first_detumble = 0; // when system detumbles for the first time, unsets this variable
+            g_first_detumble = 0; // when system detumbles for the first time, unsets this variable
         }
         if (!g_first_detumble) // if this var is unset, the system does not do anything at night
         {
