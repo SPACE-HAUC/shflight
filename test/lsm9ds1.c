ff7720cc8ff93111e24225810806d83c3e481b92
Sunip Mukherjee <sunipkmukherjee@gmail.com>
2020-02-20 19:53:37 -0500

    Disabled CSS

--- test/lsm9ds1.c
+++ test/lsm9ds1.c
@@ -132,6 +132,7 @@ int lsm9ds1_read_mag(lsm9ds1 *dev, short *B)
     for (int i = 0; i < 3; i++)
     {
         B[i] = 0; // initialize with 0
+        printf("In read_mag %d\n", __LINE__);
         buf = ++reg; // insert the command into buffer
         int wr = write(dev->mag_file, &buf, 1);
         printf("In read_mag %d\n", __LINE__);
